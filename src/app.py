e058479109b620acf2d8734f4ef12f1157fc8754
Emilian BALANESCU <emilian.balanescu@gmail.com>
2025-06-06 19:28:31 +0300

    Update app.py

--- src/app.py
+++ src/app.py
@@ -2,9 +2,7 @@ from flask import Flask, jsonify
 import datetime
 import socket
 
-
 app = Flask(__name__)
-
 
 @app.route('/api/v1/info')
 
@@ -19,10 +17,8 @@ def info():
 @app.route('/api/v1/healthz')
 
 def health():
-	# Do an actual check here
+    # Do an actual check here
     return jsonify({'status': 'up'}), 200
 
 if __name__ == '__main__':
-
     app.run(host="0.0.0.0")
-
